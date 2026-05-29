2db3196fc9d2e72ea1bdd28c201c1add3d2381b6
rodbdev <64252057+rodbdev@users.noreply.github.com>
2026-05-29 19:48:32 -0300

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "Conexion Digital - Cooperativa de Servicios",
   description: "Buscador de especialistas técnicos de la cooperativa. Electricidad, plomería, gasista, aires acondicionados, herrería y más.",
   icons: {
-    icon: 'favicon.ico',
+    icon: 'favicon.png',
   },
 };
 
