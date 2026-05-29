7940c4c1843de117f6f6b93203756dec9a4623c8
rodbdev <64252057+rodbdev@users.noreply.github.com>
2026-05-29 19:45:09 -0300

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Conexion Digital - Cooperativa de Servicios",
   description: "Buscador de especialistas técnicos de la cooperativa. Electricidad, plomería, gasista, aires acondicionados, herrería y más.",
+  icons: {
+    icon: 'favicon.ico',
+  },
 };
 
 export default function RootLayout({
